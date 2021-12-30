1442adeb74a0e5626f90ec0c96b1abadf94fe45a
Luke Walker <space3334@gmail.com>
2021-12-30 00:52:10 -0700

    added back comment in test file

--- testing/test.py
+++ testing/test.py
@@ -2,6 +2,7 @@ import random
 computer = random.randint(1, 10)
 user = -1
 while user != computer :
+	# user = int(input("Enter a number: "))
 	user = random.randint(1, 10)
 	if user < computer :
 		print(True)
